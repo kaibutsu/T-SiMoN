592364915a91661b80364ef6466bc5a6041dcef8
Sven <kaibutsu@posteo.org>
2023-03-02 15:21:17 +0100

    First mockup of simulator view

--- static/js/monitor.js
+++ static/js/monitor.js
@@ -2,98 +2,8 @@ ractive = new Ractive({
     target: '#ractive-target',
     template: '#ractive-template',
     data: {
-        patient: {
-            firstName: 'John',
-            surname: 'Doe',
-            dob: new Date('1970-01-01T00:00:00'),
-            pid: '123456789',
-        },
-        vitals: {
-            hfEcg: {
-                id: 'hfEcg',
-                title: 'HF<sub>ECG</sub> (bpm)',
-                target: 60,
-                min: 0,
-                max: 230,
-                varFreq: 1000,
-                varAmp: 1,
-                initDelay: 1000,
-                trigger: 'ecg',
-                override: '',
-            },
-            hfPleth: {
-                id: 'hfPleth',
-                title: 'HF<sub>Pleth</sub> (bpm)',
-                target: 60,
-                min: 0,
-                max: 230,
-                varFreq: 2000,
-                varAmp: 2,
-                initDelay: 10000,
-                trigger: 'pleth',
-                override: 'hfEcg',
-            },
-            pleth: {
-                id: 'pleth',
-                title: 'SpO<sub>2</sub> (%)',
-                target: 100,
-                min: 21,
-                max: 100,
-                varFreq: 2000,
-                varAmp: 1,
-                initDelay: 10000,
-                trigger: 'pleth',
-                override: '',
-            },
-            resp: {
-                id: 'resp',
-                title: 'Resp (bpm)',
-                target: 14,
-                min: 0,
-                max: 180,
-                varFreq: 5000,
-                varAmp: 5,
-                initDelay: 3000,
-                trigger: 'resp',
-                override: '',
-            },
-            nbpSys: {
-                id: 'nbpSys',
-                title: 'NPB<sub>SYS</sub> (mmHg)',
-                target: 120,
-                min: 30,
-                max: 220,
-                varFreq: 10000,
-                varAmp: null,
-                initDelay: 30000,
-                trigger: 'nbp',
-                override: '',
-            },
-            nbpDia: {
-                id: 'nbpDia',
-                title: 'NPB<sub>DIA</sub> (mmHg)',
-                target: 60,
-                min: 20,
-                max: 100,
-                varFreq: 10000,
-                varAmp: null,
-                initDelay: 30000,
-                trigger: 'nbp',
-                override: '',
-            },
-            temp: {
-                id: 'temp',
-                title: 'Temp (&deg;C)',
-                target: 37.5,
-                min: 36.0,
-                max: 41.5,
-                varFreq: 5000,
-                varAmp: 0.5,
-                initDelay: 30000,
-                trigger: 'temp',
-                override: '',
-            },
-        },
+        patient: fallbackPatient,
+        vitals: fallbackVitals,
         display: {
             colors: {
                 ecg: 'lawngreen',
@@ -122,15 +32,8 @@ ractive = new Ractive({
                     trigger: 'resp',
                 },
             },
-            triggers: {
-                ecg: true,
-                pleth: true,
-                resp: false,
-                nbp: false,
-                temp: false,
-                rBeep: false,
-            },
         },
+        triggers: fallbackTriggers,
         connection: {
             status: 'disconnected',
             peerId: '',
@@ -160,7 +63,7 @@ peer.on('connection', (c) => {
     });
 });
 
-ractive.observe('display.triggers.*', (newValue, oldValue, keypath) => {
+ractive.observe('triggers.*', (newValue, oldValue, keypath) => {
     triggerName = keypath.split('.').pop();
     Object.entries(ractive.get('display.signals')).forEach(([signalName, signalDef]) => {
         if (signalDef.trigger === triggerName) {
@@ -202,7 +105,7 @@ if (!signalUpdateTimer) {
             animateSignal(signalName)
         });
         ecgBufferPointer = bufferPointers['ecg']
-        if (ractive.get('display.triggers.rBeep') & (!(ecgBufferPointer.pos < ecgBufferPointer.size))) {
+        if (ractive.get('triggers.rBeep') & (!(ecgBufferPointer.pos < ecgBufferPointer.size))) {
             beep(
                 150,
                 220 + (220 / 100 * ractive.get('display.pleth')),
---
+++ static/js/simulator.js
@@ -0,0 +1,39 @@
+
+function peerConnect() {
+    console.log("test")
+}
+
+ractive = new Ractive({
+    target: '#ractive-target',
+    template: '#ractive-template',
+    data: {
+        patient: {},
+        vitals: {},
+        connection: {
+            status: 'disconnected',
+            peerId: '',
+            peers: [],
+        }
+    }
+});
+
+var peer = new Peer(generateWordString(wordStringLength));
+let peerConection = null;
+
+peer.on('open', (id) => {
+    ractive.set('connection.peerId', id);
+});
+
+peer.on('connection', (c) => {
+    peerConection = c;
+
+    ractive.set(['connection.status', 'connection.peers'],
+        [
+            "connected",
+            ractive.get('connection.peers').push(peerConection.peer),
+        ])
+
+    peerConection.on('data', (data) => {
+        // Work with data...
+    });
+});
